1100cc1fe91ffe43a81cbe5bde20c983d2150791
qcuong57 <hoquoccuong945@gmail.com>
2025-02-24 08:06:22 +0700

    qcuong commit

---
+++ data/user-data.js
@@ -0,0 +1,9 @@
+export const userData = {
+  githubUser: 'qcuong57',
+  devUsername: "Hồ Quốc Cường",
+  github: 'https://github.com/qcuong57',
+  facebook: 'https://www.facebook.com/gnoucdasick/',
+  linkedIn: 'https://www.linkedin.com/in/quoc-cuong-ho-b4b781335/',
+  skills: ['React', 'Redux', 'MySql', 'Postgres', 'Docker', 'AWS', 'Springboot', 'Java', '.NET'],
+  timezone: '+7'
+};
