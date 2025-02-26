b364e06e5b9e86f6592e1f7a33bc46b64875a494
qcuong57 <hoquoccuong945@gmail.com>
2025-02-26 09:53:36 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
-const path = require('path')
- 
+const path = require("path");
+
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      "media.dev.to",
+      "avatars.githubusercontent.com",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "stardev.io",
+      "github-profile-summary-cards.vercel.app",
+      "github-profile-trophy.vercel.app",
+    ],
     dangerouslyAllowSVG: true,
-  },
-  unoptimized: true,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -19,4 +26,5 @@ module.exports = {
         hostname: "**.herokuapp.com",
       },
     ],
-}
+  },
+};
